61a4bb415e0c12864de3444e679fc95bc959d09a
simple-sifu <kattyntommy@gmail.com>
2022-12-27 16:04:06 -0500

    initial version

---
+++ data-structures/hashTable.js
@@ -0,0 +1,37 @@
+function HashTable() {
+    this.SIZE = 16;
+  
+    // the array will be instantiated as [undefined, undefined....]
+    // pop() and push() shouldn't be used on the storage
+    this.storage = new Array(this.SIZE);
+  }
+  
+  // stores a value in the storage array
+  // hint: use the hash function to determine where in the array to store the value
+  HashTable.prototype.set = function(key, value) {
+  
+  };
+  
+  // return a previously stored value
+  HashTable.prototype.get = function(key) {
+  
+  };
+  
+  // returns and removes a key from the hash table
+  HashTable.prototype.remove = function(key) {
+  
+  };
+  
+  // returns a number between 0 and size that is unique* and generated from the the inputted string
+  function hashCode(string, size){
+    let hash = 0;
+    if (string.length == 0) return hash;
+    for (let i = 0; i < string.length; i++) {
+      const letter = string.charCodeAt(i);
+      hash = ((hash << 5) - hash) + letter;
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    return Math.abs(hash) % size ;
+  }
+
+  
